ddde3f1aa02536a7c589625946c7e61ff889625e
Vasu Sahu <Vasu.sahu.8080@gmail.com>
2025-10-11 21:25:51 +0530

    Added Desired Modal Component

---
+++ src/app/components/Modal.js
@@ -0,0 +1,19 @@
+"use client";
+
+export default function Modal({ isOpen, onClose, children }) {
+  if (!isOpen) return null;
+
+  return (
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+      onClick={onClose}
+    >
+      <div
+        className="bg-white p-6 rounded shadow-lg"
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
